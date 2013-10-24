960f2bbb4ae37f4e09402dc9885ddd28f9f94a0c
Nabeel Ahmad <nabeel.ahmad.lhr@gmail.com>
2013-10-24 14:20:38 +0500

    app.js

--- cloud/app.js
+++ cloud/app.js
@@ -1,5 +1,3 @@
-
-// These two lines are required to initialize Express in Cloud Code.
 var express = require('express');
 var moment = require('moment');
 var _ = require('underscore');
@@ -70,18 +68,3 @@ app.get('/activity', function(req, res) {
 		}
 	});
 });
-
-// // Example reading from the request query string of an HTTP get request.
-// app.get('/test', function(req, res) {
-//   // GET http://example.parseapp.com/test?message=hello
-//   res.send(req.query.message);
-// });
-
-// // Example reading from the request body of an HTTP post request.
-// app.post('/test', function(req, res) {
-//   // POST http://example.parseapp.com/test (with request body "message=hello")
-//   res.send(req.body.message);
-// });
-
-// Attach the Express app to Cloud Code.
-app.listen();
